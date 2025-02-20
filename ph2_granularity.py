083b4ac08cdcd0623a93a26c3b6b9df5482c366e
alexandertuna <alexander.tuna@gmail.com>
2025-02-20 15:51:16 -0800

    Add a few more plots

--- ph2_granularity.py
+++ ph2_granularity.py
@@ -51,10 +51,13 @@ class Plotter:
             #self.plot_rz_order(pdf)
             #self.plot_rz_side(pdf)
             #self.plot_rz_layer(pdf)
-            self.plot_rod_xy_1to4(pdf)
-            self.plot_rod_xy_all(pdf)
-            self.plot_rod_xy_number(pdf)
-            self.plot_rod_rz(pdf)
+            #self.plot_rod_xy_1to4(pdf)
+            #self.plot_rod_xy_all(pdf)
+            #self.plot_rod_xy_number(pdf)
+            #self.plot_rod_rz(pdf)
+            #self.plot_module_rz(pdf)
+            #self.plot_module_rz_number(pdf)
+            self.plot_rod_rz_tilted(pdf)
 
 
     def plot_rz_order(self, pdf: PdfPages) -> None:
@@ -235,6 +238,121 @@ class Plotter:
         ax.text(0.85, 1.02, f"ph2_side = {side}", transform=ax.transAxes, fontsize=8)
         ax.text(0.03, 0.01, f"rod%2=1", color=colors[1], transform=ax.transAxes, fontsize=16)
         ax.text(0.03, 0.06, f"rod%2=0", color=colors[2], transform=ax.transAxes, fontsize=16)
+        fig.subplots_adjust(bottom=0.12, left=0.18, right=0.96, top=0.95)
+        pdf.savefig()
+        plt.close()
+
+    def plot_module_rz(self, pdf: PdfPages) -> None:
+        name = "ph2_module"
+        side = 3
+        layer = 1
+        colors = {
+            1: "tab:blue",
+            2: "tab:orange",
+            3: "tab:green",
+            4: "tab:red",
+            5: "tab:purple",
+            6: "tab:brown",
+            7: "tab:pink",
+        }
+        fig, ax = plt.subplots(figsize=(8, 6))
+        for key in colors:
+            subset = self.data[(self.data[name] == key) & (self.data["ph2_layer"] == layer) & (self.data["ph2_side"] == side)]
+            ax.scatter(subset["ph2_z"], subset["ph2_r"], s=1, c=colors[key], marker=".", edgecolors='none')
+        ax.set_xlabel("z [cm]")
+        ax.set_ylabel("r [cm]")
+        ax.set_xlim(-18, 18)
+        ax.set_ylim(21, 26)
+        ax.tick_params(right=True, top=True)
+        ax.text(0.00, 1.02, BNAME, transform=ax.transAxes, fontsize=8)
+        ax.text(0.80, 1.02, f"side = {side}, layer = {layer}", transform=ax.transAxes, fontsize=8)
+        ax.text(0.00, 0.53, f"module", color="black", transform=ax.transAxes, fontsize=24)
+        ax.text(0.10, 0.47, f"1", color=colors[1], transform=ax.transAxes, fontsize=24)
+        ax.text(0.23, 0.47, f"2", color=colors[2], transform=ax.transAxes, fontsize=24)
+        ax.text(0.35, 0.47, f"3", color=colors[3], transform=ax.transAxes, fontsize=24)
+        ax.text(0.48, 0.47, f"4", color=colors[4], transform=ax.transAxes, fontsize=24)
+        ax.text(0.60, 0.47, f"5", color=colors[5], transform=ax.transAxes, fontsize=24)
+        ax.text(0.73, 0.47, f"6", color=colors[6], transform=ax.transAxes, fontsize=24)
+        ax.text(0.85, 0.47, f"7", color=colors[7], transform=ax.transAxes, fontsize=24)
+        fig.subplots_adjust(bottom=0.12, left=0.18, right=0.96, top=0.95)
+        pdf.savefig()
+        plt.close()
+
+
+    def plot_module_rz_number(self, pdf: PdfPages) -> None:
+        name = "ph2_module"
+        side = 3
+        colors = {
+            1: "tab:blue",
+            2: "tab:orange",
+            3: "tab:green",
+            4: "tab:red",
+            5: "tab:purple",
+            6: "tab:brown",
+            0: "tab:pink",
+        }
+        fig, ax = plt.subplots(figsize=(8, 6))
+        layer_min = self.data["ph2_layer"].min()
+        layer_max = self.data["ph2_layer"].max()
+        for layer in range(layer_min, layer_max+1):
+            subset_layer = self.data[(self.data["ph2_layer"] == layer) & (self.data["ph2_side"] == side)]
+            mod_min = subset_layer[name].min()
+            mod_max = subset_layer[name].max()
+            for mod in range(mod_min, mod_max+1):
+                subset = subset_layer[subset_layer[name] == mod]
+                fontsize = 10 if layer > 3 else 6
+                ax.text(subset["ph2_z"].mean(), subset["ph2_r"].mean(), f"{mod}", fontsize=fontsize, color=colors[mod % len(colors)], ha="center", va="center")
+        ax.set_xlabel("z [cm]")
+        ax.set_ylabel("r [cm]")
+        ax.set_xlim(-130, 130)
+        ax.set_ylim(10, 120)
+        ax.tick_params(right=True, top=True)
+        ax.text(0.00, 1.02, BNAME, transform=ax.transAxes, fontsize=8)
+        ax.text(0.85, 1.02, f"ph2_side = {side}", transform=ax.transAxes, fontsize=8)
+        ax.text(0.03, 0.04, f"module number", transform=ax.transAxes, fontsize=20)
+        fig.subplots_adjust(bottom=0.12, left=0.18, right=0.96, top=0.95)
+        pdf.savefig()
+        plt.close()
+
+
+    def plot_rod_rz_tilted(self, pdf: PdfPages) -> None:
+        name = "ph2_rod"
+        sides = (1, 2)
+        order = 0
+        colors = {
+            1: "tab:blue",
+            2: "tab:orange",
+            3: "tab:green",
+            4: "tab:red",
+            5: "tab:purple",
+            6: "tab:brown",
+            7: "tab:pink",
+            8: "tab:gray",
+            9: "tab:olive",
+            10: "tab:cyan",
+            11: "lime",
+            12: "black",
+        }
+        fig, ax = plt.subplots(figsize=(8, 6))
+        mask = np.isin(self.data["ph2_side"], sides) & (self.data["ph2_order"] == order)
+        for key in colors:
+            subset = self.data[mask & (self.data[name] == key)]
+            ax.scatter(subset["ph2_z"], subset["ph2_r"], s=1, c=colors[key], marker=".", edgecolors='none')
+
+            xpos_l = subset["ph2_z"][(subset["ph2_layer"] == 2) & (subset["ph2_z"] < 0)].mean()
+            xpos_r = subset["ph2_z"][(subset["ph2_layer"] == 2) & (subset["ph2_z"] > 0)].mean()
+            ypos = 42 + (1.5 if key % 2 == 1 else 0)
+            ax.text(xpos_l, ypos, key, color=colors[key], fontsize=14, ha="center")
+            ax.text(xpos_r, ypos, key, color=colors[key], fontsize=14, ha="center")
+
+        ax.set_xlabel("z [cm]")
+        ax.set_ylabel("r [cm]")
+        ax.set_xlim(-130, 130)
+        ax.set_ylim(20, 60)
+        ax.tick_params(right=True, top=True)
+        ax.text(0.00, 1.02, BNAME, transform=ax.transAxes, fontsize=8)
+        ax.text(0.75, 1.02, f"side = {sides}, order = {order}", transform=ax.transAxes, fontsize=8)
+        ax.text(0.00, 0.63, f"module", color="black", transform=ax.transAxes, fontsize=20)
         fig.subplots_adjust(bottom=0.12, left=0.18, right=0.96, top=0.95)
         pdf.savefig()
         plt.close()
